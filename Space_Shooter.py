054599cc8449b17aade080013c0bcd5d4c90e953
Tornrind <137106134+Tornrind@users.noreply.github.com>
2023-12-05 14:54:25 +0100

    Work distributed

---
+++ Space_Shooter.py
@@ -0,0 +1,11 @@
+# Each of us needs to code two classes
+Game #Michael
+Spaceship #Gina
+Powerup #Fabian
+Weapon #Fabian
+Hitbox #Michael
+BigShotWeapon #Panchita
+DefaultWeapon #Panchita
+Bullet #Gina
+BigAsteroid #Florian
+SmallAsteroid #Florian
