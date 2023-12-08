1c14ad96b1c7d587e29c797e89f2c02260cdd7a4
Tornrind <137106134+Tornrind@users.noreply.github.com>
2023-12-08 08:37:06 +0100

    Work distributed

--- powerup.py
+++
@@ -1,4 +0,0 @@
-class Powerup:
-    def__init__(self, x:float, y:float, sprite, speed:float)
-    def move(self, x:float, y:float):
-        pass
--- weapon.py
+++
@@ -1,4 +0,0 @@
-class Weapon:
-    def__init__(self, reload_time:float):
-    def shoot(self, x:float, y:float):
-        pass
