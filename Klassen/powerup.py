efa4e0ba047623f2d3c21ff467c1a34ae2130766
Tornrind <137106134+Tornrind@users.noreply.github.com>
2023-12-08 08:49:51 +0100

    Work distributed

---
+++ Klassen/powerup.py
@@ -0,0 +1,8 @@
+class Powerup:
+    def __init__(self, x:float, y:float, sprite, speed:float):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.speed = speed
+    def move(self, x:float, y:float):
+        pass
