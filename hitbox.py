d099f25985984cb8813b5cc5ecce20cc5448d16c
Michael Sommer <150002657+mso-sgr@users.noreply.github.com>
2023-12-08 09:01:57 +0100

    Delete hitbox.py

--- hitbox.py
+++
@@ -1,6 +0,0 @@
-class Hitbox:
-    def __init__(self, x, y, width, height):
-        self.x = x  # X-Koordinate der oberen linken Ecke der Hitbox
-        self.y = y  # Y-Koordinate der oberen linken Ecke der Hitbox
-        self.width = width  # Breite der Hitbox
-        self.height = height  # Höhe der Hitbox
