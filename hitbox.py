cdfb378217b4a03f9df1e94b4328a6c532734483
Michael Sommer <150002657+mso-sgr@users.noreply.github.com>
2023-12-08 09:05:12 +0100

    Add files via upload

---
+++ hitbox.py
@@ -0,0 +1,5 @@
+class Hitbox:
+    def __init__(self, x, y, radius):
+        self.x = x  # X-Koordinate der oberen linken Ecke der Hitbox
+        self.y = y  # Y-Koordinate der oberen linken Ecke der Hitbox
+        self.radius = radius  # Radius der Hitbox
