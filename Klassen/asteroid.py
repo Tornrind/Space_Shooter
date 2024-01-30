0915aa2ab4cb149fd6d5032d7ce7c0afe6d12e4e
panchitaZa <152987143+panchitaZa@users.noreply.github.com>
2024-01-30 12:48:28 +0100

    Asteroid änderung

---
+++ Klassen/asteroid.py
@@ -0,0 +1,10 @@
+class Asteroid:
+    def __init__(self, x: float, y: float, speed: float, sprite: Sprite, split_probability: float, healthpoints: int):
+        self.x = x
+        self.y = y
+        self.sprite = Sprite
+        self.speed = speed
+        self.healthpoints = healthpoints
+
+        def move(self, y: int, x: int):
+            pass
